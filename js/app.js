d66650beb74101709438e8de54bb1dd9f99d5b2d
Charlie Arriaga <arriayaa@gmail.com>
2023-10-30 14:27:26 -0700

    created hourly customer functions

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 'use strict'
 
+function getRandomNumberInt (min, max) {
+    let span = max - min + 1;
+    let randomNumber = Math.floor(Math.random() * span);
+    let returnNumber = randomNumber + min;
+    //console.log (returnNumber);
+    return returnNumber;
+}
+
+function hourlyCustomer (hourTotal, min, max) {
+    let hourArray = []
+    for (let i=0; i<13; i++) {
+        let customer = getRandomNumberInt (min, max)
+        hourArray.push(customer)
+    }
+    return hourArray
+}
+
+
+
+const seattleLocation = {
+   
+}
+
+const tokyoLocation = {
+
+}
